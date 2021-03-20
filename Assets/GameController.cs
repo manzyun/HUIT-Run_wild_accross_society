2d57c207f87c8e318737f01deb3915282c5551f1
manimani <manichado@icloud.com>
2021-03-20 13:36:33 +0900

    ゴールしたらタイマー止める

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -10,23 +10,26 @@ public class GameController : MonoBehaviour
     float goal_time;
     // public GameObject MainText;
     public Text MainText;
+    bool goal_bool;
 
     // Use this for initialization
     void Start () {
+        goal_bool = false;
         // MainText.SetActive (false);
     }
     
     // Update is called once per frame
     void Update () {
-        MainText.text = Math.Round(Time.time, 1,  MidpointRounding.AwayFromZero).ToString();    
+        if (!goal_bool){
+            MainText.text = Math.Round(Time.time, 1,  MidpointRounding.AwayFromZero).ToString(); 
+        }
     }
 
 
     void OnTriggerEnter(Collider collider){
         if (collider.gameObject.name == "bear") {
-            Debug.Log ("ゴール！");
-
-            // MainText.SetActive (true);
+            Debug.Log ("ゴール！");    
+            goal_bool = true; 
         }
     }
  
