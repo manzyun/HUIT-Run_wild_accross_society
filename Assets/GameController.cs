68b8dec082845848d349fd5e2ded30016d0782aa
manimani <manichado@icloud.com>
2021-03-20 13:24:41 +0900

    タイマー追加

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,39 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject MainText;
+
+    float goal_time;
+    // public GameObject MainText;
+    public Text MainText;
+
     // Use this for initialization
     void Start () {
-        MainText.SetActive (false);
+        // MainText.SetActive (false);
     }
     
     // Update is called once per frame
     void Update () {
-        
+        MainText.text = Math.Round(Time.time, 1,  MidpointRounding.AwayFromZero).ToString();    
     }
 
 
     void OnTriggerEnter(Collider collider){
         if (collider.gameObject.name == "bear") {
             Debug.Log ("ゴール！");
-            MainText.SetActive (true);        }
+
+            // MainText.SetActive (true);
+        }
     }
  
 
-    // // 衝突した瞬間に呼ばれる  
-    // void OnCollisionEnter(Collision collision) {  
-    //     Debug.Log ("OnColider1");
-    // }  
-    // // 衝突している間呼ばれ続ける  
-    // void OnCollisionStay(Collision collision) {  
-    //     Debug.Log ("OnColider2");
-    // }  
-    // // 衝突から離れた瞬間に呼ばれる  
-    // void OnCollisionExit(Collision collision) {  
-    //     Debug.Log ("OnColider3");
-    // }  
+  
+
 }
 
